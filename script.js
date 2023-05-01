7271310511b97a136c789cefa1957bd99107aa3f
JuliaBel5 <juliabel5@gmail.com>
2023-05-02 02:28:09 +0300

    fix: fix quotes errors

--- script.js
+++ script.js
@@ -36,61 +36,60 @@ function init() {
     let kboard = '';
     for (let i = 0; i < arrKey.length; i++) {
         if (i === 13) {
-            kboard = kboard + '<div class='keyboard__key keyboard__key_service' data='' + 'Backspace' + '' >' + 'Backspace' + '</div>' + '<div class = 'clearfix'></div>';
+            kboard = kboard + '<div class="keyboard__key keyboard__key_service" data="' + "Backspace" + '" >' + "Backspace" + '</div>' + '<div class = "clearfix"></div>';
         } else if (i === 28) {
-            kboard = kboard + '<div class = 'clearfix'></div>' + '<div class='keyboard__key keyboard__key_service keyboard__key_capslock' data='' + 'CapsLock' + '' >' + 'Caps Lock' + '</div>';
+            kboard = kboard + '<div class = "clearfix"></div>' + '<div class="keyboard__key keyboard__key_service keyboard__key_capslock" data="' + "CapsLock" + '" >' + "Caps Lock" + '</div>';
         } else if (i === 39) {
-            kboard = kboard + '<div class='keyboard__key keyboard__key_service' data='' + 'Enter' + '' >' + 'Enter' + '</div>' + '<div class = 'clearfix'></div>' + '<div class='keyboard__key keyboard__key_service' data='' + 'ShiftLeft' + '' >' + 'Shift' + '</div>';
+            kboard = kboard + '<div class="keyboard__key keyboard__key_service" data="' + "Enter" + '" >' + "Enter" + '</div>' + '<div class = "clearfix"></div>' + '<div class="keyboard__key keyboard__key_service" data="' + "ShiftLeft" + '" >' + "Shift" + '</div>';
         } else if (i === 50) {
-            kboard = kboard + '<div class='keyboard__key keyboard__key_service' data='' + 'ShiftRight' + '' >' + 'Shift' + '</div>' + '<div class = 'clearfix'></div>'
+            kboard = kboard + '<div class="keyboard__key keyboard__key_service" data="' + "ShiftRight" + '" >' + "Shift" + '</div>' + '<div class = "clearfix"></div>'
         } else if (i === 53) {
-            kboard = kboard + '<div class='keyboard__key keyboard__key_space' data='' + 'Space' + '' >' + ' ' + '</div>';
+            kboard = kboard + '<div class="keyboard__key keyboard__key_space" data="' + "Space" + '" >' + " " + '</div>';
         }
 
-        kboard += '<div class='keyboard__key' data='' + arrCode[i] + '' >' + arrKey[i] + '</div>';
+        kboard += '<div class="keyboard__key" data="' + arrCode[i] + '" >' + arrKey[i] + '</div>';
     }
 
     keyboard.innerHTML = kboard;
-    keyboard.classList.add('keyboard')
+    keyboard.classList.add("keyboard")
     textArea.classList.add('textarea')
-    document.querySelector('body').appendChild(keyboard);
-    document.querySelector('body').appendChild(textArea);
+    document.querySelector("body").appendChild(keyboard);
+    document.querySelector("body").appendChild(textArea);
 
-    // document.querySelector('.keyboard').innerHTML = kboard;
+    // document.querySelector(".keyboard").innerHTML = kboard;
 
 }
 init();
 
 
-document.querySelector('.keyboard__key[data='Tab']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='MetaLeft']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='Delete']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='ControlLeft']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='ControlRight']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='AltLeft']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='AltRight']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='ArrowLeft']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='ArrowLeft']').classList.add('keyboard__key_arrow');
-document.querySelector('.keyboard__key[data='ArrowUp']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='ArrowUp']').classList.add('keyboard__key_arrow');
-document.querySelector('.keyboard__key[data='ArrowRight']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='ArrowRight']').classList.add('keyboard__key_arrow');
-document.querySelector('.keyboard__key[data='ArrowDown']').classList.add('keyboard__key_service-small');
-document.querySelector('.keyboard__key[data='ArrowDown']').classList.add('keyboard__key_arrow');
+document.querySelector('.keyboard__key[data="Tab"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="MetaLeft"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="Delete"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="ControlLeft"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="ControlRight"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="AltLeft"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="AltRight"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="ArrowLeft"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="ArrowLeft"]').classList.add('keyboard__key_arrow');
+document.querySelector('.keyboard__key[data="ArrowUp"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="ArrowUp"]').classList.add('keyboard__key_arrow');
+document.querySelector('.keyboard__key[data="ArrowRight"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="ArrowRight"]').classList.add('keyboard__key_arrow');
+document.querySelector('.keyboard__key[data="ArrowDown"]').classList.add('keyboard__key_service-small');
+document.querySelector('.keyboard__key[data="ArrowDown"]').classList.add('keyboard__key_arrow');
 
 
-const keyBackspace = document.querySelector('.keyboard .keyboard__key[data='Backspace']');
-const keyDelete = document.querySelector('.keyboard .keyboard__key[data='Delete']');
-const keyTab = document.querySelector('.keyboard .keyboard__key[data='Tab']');
-const keyEnter = document.querySelector('.keyboard .keyboard__key[data='Enter']');
-const keyShiftLeft = document.querySelector('.keyboard .keyboard__key[data='ShiftLeft']');
-const keyShiftRight = document.querySelector('.keyboard .keyboard__key[data='ShiftRight']');
-const keyCapsLock = document.querySelector('.keyboard .keyboard__key[data='CapsLock']');
-const keyCtrlLeft = document.querySelector('.keyboard .keyboard__key[data='ControlLeft']');
-const keyCtrlRight = document.querySelector('.keyboard .keyboard__key[data='ControlRight']');
-const keyAltLeft = document.querySelector('.keyboard .keyboard__key[data='AltLeft']');
-const keyAltRight = document.querySelector('.keyboard .keyboard__key[data='AltRight']');
-
+const keyBackspace = document.querySelector('.keyboard .keyboard__key[data="Backspace"]');
+const keyDelete = document.querySelector('.keyboard .keyboard__key[data="Delete"]');
+const keyTab = document.querySelector('.keyboard .keyboard__key[data="Tab"]');
+const keyEnter = document.querySelector('.keyboard .keyboard__key[data="Enter"]');
+const keyShiftLeft = document.querySelector('.keyboard .keyboard__key[data="ShiftLeft"]');
+const keyShiftRight = document.querySelector('.keyboard .keyboard__key[data="ShiftRight"]');
+const keyCapsLock = document.querySelector('.keyboard .keyboard__key[data="CapsLock"]');
+const keyCtrlLeft = document.querySelector('.keyboard .keyboard__key[data="ControlLeft"]');
+const keyCtrlRight = document.querySelector('.keyboard .keyboard__key[data="ControlRight"]');
+const keyAltLeft = document.querySelector('.keyboard .keyboard__key[data="AltLeft"]');
+const keyAltRight = document.querySelector('.keyboard .keyboard__key[data="AltRight"]');
 
 
 
@@ -118,40 +117,40 @@ document.onkeydown = function (event) {
     console.log(ind);
 
 
-    document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').classList.add('active');
+    document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').classList.add('active');
   
-    if (event.getModifierState('CapsLock')) {
+    if (event.getModifierState("CapsLock")) {
         console.log('Ctrl');
-        if (!((document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').classList.contains('keyboard__key_service-small')) || (document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').classList.contains('keyboard__key_service')))) {
+        if (!((document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').classList.contains('keyboard__key_service-small')) || (document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').classList.contains('keyboard__key_service')))) {
 
-            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').textContent.toUpperCase() + textarea.textContent.slice(ind);
+            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').textContent.toUpperCase() + textarea.textContent.slice(ind);
             ind = ind + 1;
             textarea.setSelectionRange(ind, ind)
 
-        } else if (document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').classList.contains('keyboard__key_arrow')) {
+        } else if (document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').classList.contains('keyboard__key_arrow')) {
 
-            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').textContent.toUpperCase() + textarea.textContent.slice(ind);
+            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').textContent.toUpperCase() + textarea.textContent.slice(ind);
             ind = ind + 1;
             textarea.setSelectionRange(ind, ind)
 
         }
     } else {
 
-        if (!((document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').classList.contains('keyboard__key_service-small')) || (document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').classList.contains('keyboard__key_service')))) {
+        if (!((document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').classList.contains('keyboard__key_service-small')) || (document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').classList.contains('keyboard__key_service')))) {
 
-            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').textContent + textarea.textContent.slice(ind);
+            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').textContent + textarea.textContent.slice(ind);
             ind = ind + 1;
             textarea.setSelectionRange(ind, ind)
 
-        } else if (document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').classList.contains('keyboard__key_arrow')) {
+        } else if (document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').classList.contains('keyboard__key_arrow')) {
 
-            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data='' + event.code + '']').textContent + textarea.textContent.slice(ind);
+            textarea.textContent = textarea.textContent.slice(0, ind) + document.querySelector('.keyboard .keyboard__key[data="' + event.code + '"]').textContent + textarea.textContent.slice(ind);
             ind = ind + 1;
             textarea.setSelectionRange(ind, ind)
 
         }
     }
-    if (event.code === 'Backspace') {
+    if (event.code === "Backspace") {
         if (ind > 0) {
             textarea.textContent = textarea.textContent.slice(0, ind - 1) + textarea.textContent.slice(ind);
             ind = ind - 1;
@@ -160,27 +159,27 @@ document.onkeydown = function (event) {
             textarea.setSelectionRange(ind, ind)
         }
     }
-    if (event.code === 'Delete') {
+    if (event.code === "Delete") {
         delNext();
 
     }
-    if (event.code === 'Tab') {
-        console.log('Tab');
+    if (event.code === "Tab") {
+        console.log("Tab");
         addInd();
     }
 
-    if (event.code === 'Enter') {
-        console.log('Enter');
+    if (event.code === "Enter") {
+        console.log("Enter");
         addEnter();
     }
 
-    if (event.code === 'CapsLock') {
+    if (event.code === "CapsLock") {
         keyCapsLock.classList.toggle('keyboard__key_capslock-light')
     }
-    if (event.code === 'ShiftLeft') {
+    if (event.code === "ShiftLeft") {
     keyShiftLeft.classList.toggle('activated');
     }
-    if (event.code === 'ShiftRight') {
+    if (event.code === "ShiftRight") {
         keyShiftLeft.classList.toggle('activated');
         
 }
